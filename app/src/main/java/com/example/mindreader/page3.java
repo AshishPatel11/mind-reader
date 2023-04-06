0dfb0beb988fbb7fcb3c2fdfd86bd523c6e7334e
shivkaran <shivkaranyadav3704@Gmail.com>
2023-04-06 11:34:52 +0530

    SHIV

--- app/src/main/java/com/example/mindreader/page3.java
+++ app/src/main/java/com/example/mindreader/page3.java
@@ -2,7 +2,10 @@ package com.example.mindreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class page3 extends AppCompatActivity {
 
@@ -10,5 +13,5 @@ public class page3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page3);
-    }
+        getSupportActionBar().hide();    }
 }
