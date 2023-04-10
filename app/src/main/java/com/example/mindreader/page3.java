5124638880e498ded1e990393cdeee61680ba805
Ashish Patel <ashishpatel287680@gmail.com>
2023-04-10 11:04:12 +0530

    lab cmt

--- app/src/main/java/com/example/mindreader/page3.java
+++ app/src/main/java/com/example/mindreader/page3.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class page3 extends AppCompatActivity {
 
@@ -23,5 +24,8 @@ public class page3 extends AppCompatActivity {
                 startActivity(nextactivity);
             }
         });
+        int name = R.drawable.spotify;
+        ImageView chart = findViewById(R.id.chart);
+        chart.setImageResource(name);
     }
 }
