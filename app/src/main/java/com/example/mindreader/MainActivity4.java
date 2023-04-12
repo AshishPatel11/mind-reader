153bb5527b823a90bbd3e700c39f2ac579865d86
dixit <varadixit@gmail.com>
2023-04-12 20:53:07 +0530

    dixit

--- app/src/main/java/com/example/mindreader/MainActivity4.java
+++ app/src/main/java/com/example/mindreader/MainActivity4.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity4 extends AppCompatActivity {
 
@@ -20,8 +23,15 @@ public class MainActivity4 extends AppCompatActivity {
         //image view
         ImageView imageView = (ImageView) findViewById(R.id.displayans);
         ImageView globe = (ImageView) findViewById(R.id.imageView2);
+        Button btn = findViewById(R.id.tryAgain);
+        TextView clickme = findViewById(R.id.clickme);
+
         Bundle bundle = getIntent().getExtras();
+
         int name[] = {R.mipmap.spotify,R.mipmap.android,R.mipmap.apple,R.mipmap.bluetooth,R.mipmap.code,R.mipmap.github,R.mipmap.google,R.mipmap.insta,R.mipmap.java,R.mipmap.js,R.mipmap.keybrd,R.mipmap.linkdin,R.mipmap.mouse,R.mipmap.msg,R.mipmap.pin,R.mipmap.portal,R.mipmap.processor,R.mipmap.python,R.mipmap.react,R.mipmap.wifi};
+
+        Animation fade = AnimationUtils.loadAnimation(this,R.anim.fade);
+        Animation fadeout = AnimationUtils.loadAnimation(this,R.anim.fadeout);
 
         globe.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,11 +41,15 @@ public class MainActivity4 extends AppCompatActivity {
                     int imageAns = bundle.getInt("imageAns");
                     imageView.setImageResource(name[imageAns]);
                 }
+                btn.setVisibility(View.VISIBLE);
+                btn.setAnimation(fade);
+                imageView.setAnimation(fade);
+                clickme.setAnimation(fadeout);
+                clickme.setVisibility(View.INVISIBLE);
             }
         });
-        Button nextbtn = findViewById(R.id.restart);
         Intent nextactivity = new Intent(MainActivity4.this,MainActivity.class);
-        nextbtn.setOnClickListener(new View.OnClickListener() {
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(nextactivity);
