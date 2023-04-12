99314a58c222ae72c61912a5785f9035eeb81db4
Ashish Patel <ashishpatel287680@gmail.com>
2023-04-12 15:35:30 +0530

    lab cmt

--- app/src/main/java/com/example/mindreader/MainActivity4.java
+++ app/src/main/java/com/example/mindreader/MainActivity4.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity4 extends AppCompatActivity {
@@ -32,7 +33,14 @@ public class MainActivity4 extends AppCompatActivity {
                 }
             }
         });
-
+        Button nextbtn = findViewById(R.id.restart);
+        Intent nextactivity = new Intent(MainActivity4.this,MainActivity.class);
+        nextbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(nextactivity);
+            }
+        });
 
 
     }
